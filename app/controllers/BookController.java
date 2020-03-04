902f7287ff37fcedfb98e971da42eb153ed57584
beni-gut <55495952+beni-gut@users.noreply.github.com>
2020-03-04 15:28:52 +0100

    Arbeitsauftrag http, Woche 2, Routes und BookController

---
+++ app/controllers/BookController.java
@@ -0,0 +1,41 @@
+package controllers;
+
+import play.mvc.*;
+
+public class BookController extends Controller {
+    public Result getAll(String q) {
+        if (q == null) {
+            return ok("getAll works");
+        } else {
+            return ok("Parameter q ist: " + q);
+        }
+    }
+
+    public Result add(Http.Request request) {
+        return ok("Nice request dude");
+    }
+
+    public Result update(Long id) {
+        if (id != null) {
+            return ok("Updated the book with id " + id);
+        } else {
+            return badRequest("Please retry with other parameters");
+        }
+    }
+
+    public Result get(Long id) {
+        if (id != null) {
+            return ok("Here's some info on book with id " + id);
+        } else {
+            return notFound("This seems to not exist");
+        }
+    }
+
+    public Result remove(Long id) {
+        if (id != null) {
+            return ok("Removed book with id " + id);
+        } else {
+            return badRequest("Please retry with other parameters");
+        }
+    }
+}
