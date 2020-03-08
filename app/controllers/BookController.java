384db1a6804c3fbfafaa926d970468d3d622b47f
beni-gut <55495952+beni-gut@users.noreply.github.com>
2020-03-08 16:01:36 +0100

    Weiterführung der Aufgabe Wo 3

--- app/controllers/BookController.java
+++ app/controllers/BookController.java
@@ -1,20 +1,36 @@
 package controllers;
 
 import play.mvc.*;
+import services.DefaultBookService;
 
 public class BookController extends Controller {
+    DefaultBookService bookService;
+
+    /**
+     * Alle Bücher abfragen
+     * @param q Suchparameter
+     */
     public Result getAll(String q) {
         if (q == null) {
             return ok("getAll works");
         } else {
+            //import method
             return ok("Parameter q ist: " + q);
         }
     }
 
+    /**
+     * Neues Buchdetail erfassen
+     * @param request
+     */
     public Result add(Http.Request request) {
         return ok("Was probably posted, it was a request " + request);
     }
 
+    /**
+     * Buchdetail für Buch mit ID aktualisieren
+     * @param id Buch ID
+     */
     public Result update(Long id) {
         if (id != null) {
             return ok("Updated the book with id " + id);
@@ -23,6 +39,10 @@ public class BookController extends Controller {
         }
     }
 
+    /**
+     * Abfragen der Buchdetails für ein Buch mit ID
+     * @param id Buch ID
+     */
     public Result get(Long id) {
         if (id != null) {
             return ok("Here's some info on book with id " + id);
@@ -31,6 +51,10 @@ public class BookController extends Controller {
         }
     }
 
+    /**
+     * Buch mit entsprechender ID löschen
+     * @param id Buch ID
+     */
     public Result remove(Long id) {
         if (id != null) {
             return ok("Removed book with id " + id);
--- app/services/DefaultBookService.java
+++ app/services/DefaultBookService.java
@@ -1,9 +1,77 @@
 package services;
 
 import models.Book;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class DefaultBookService {
     ArrayList<Book> bookList = new ArrayList<Book>();
+
+    public DefaultBookService() {}
+
+    /**
+     * Return's list of all books.
+     *
+     * @return list of all books
+     */
+    List<Book> get() {
+        return bookList;
+    }
+
+    /**
+     * Returns book with given identifier.
+     *
+     * @param id book identifier
+     * @return book with given identifier or {@code null}
+     */
+    Book get(final Long id) {
+        for (Book book: bookList ) {
+            if (book.getId() == id) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Removes book with given identifier.
+     *
+     * @param id book identifier
+     * @return {@code true} on success  {@code false} on failure
+     */
+    boolean delete(final Long id) {
+        for (Book book: bookList ) {
+            if (book.getId() == id) {
+                bookList.remove(book);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Updates book with given identifier.
+     *
+     * @param updatedBook book with updated fields
+     * @return updated book
+     */
+    Book update(final Book updatedBook) {
+        for (Book book: bookList ) {
+            if (book.getId() == updatedBook.getId()) {
+                bookList.set(bookList.indexOf(book), updatedBook);
+                return book;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Adds the given book.
+     *
+     * @param book to add
+     * @return added book
+     */
+    Book add(final Book book) {
+        bookList.add(book);
+        return book;
+    }
 }
